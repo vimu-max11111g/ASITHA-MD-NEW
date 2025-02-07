9edf2cba7cd950ef9b0f8dfa9641b198074475f0
vimu-max11111g <foxgang1122@gmail.com>
2025-02-07 09:04:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yip3nQwQ#76dDqiuuHPQqdFSko-r6IfzDGiza4C6w7ZCMqb2DloQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=W7ZyVDIB#wbgnC7AbwJjRVAiUZBLf7OBYIsnDrYPpbNAe_9jlUT8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
